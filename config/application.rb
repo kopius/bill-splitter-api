bfa6b89576aa3597a193cefa80ef07b5c01a1692
kopiusmaximus <alexander.kopynec@gmail.com>
2016-10-28 16:35:39 -0400

    Change backup client origin to localhost:3000

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module BillSplitter
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_ORIGIN'] || 'https://kopiusmaximus.github.io'
+        origins ENV['CLIENT_ORIGIN'] || 'http://localhost:8080'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
